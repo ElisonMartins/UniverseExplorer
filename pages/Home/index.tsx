44b507f2e0b8d3740d6806077cfcde60f6c3fb72
elisonmartins <martinselison10@gmail.com>
2023-05-16 21:06:39 -0300

    fix: tentando corrigir erro

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,69 +1,66 @@
 "use client"; // This is a client component 
-import { useEffect, useState} from 'react'
+import { useEffect, useState } from 'react';
 
-function Home() {
-  const [isConnected, setConnectedStatus] = useState(false)
-  const [status, setStatus] = useState('')
-  const [walletAddress, setWallet] = useState('')
-
-  const connectWalletPressed = async () => {
-    if (isConnected)
-      return alert(
-        "Conta já conectada! " +
-          String(walletAddress).substring(0, 5) +
-          "..." +
-          String(walletAddress).substring(38)
-      )
-
-    const walletResponse = await connectWallet()
-    setConnectedStatus(walletResponse.connectedStatus)
-    setStatus(walletResponse.status)
-    setWallet(walletResponse.address)
-  }
+const Home = () => {
+  const [isConnected, setConnected] = useState(false);
+  const [status, setStatus] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
 
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
-        const address = await window.ethereum.enable()
-        const obj = {
-          connectedStatus: true,
-          status: "Conectado",
-          address: address,
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        if (accounts && accounts.length > 0) {
+          const address = accounts[0];
+          setConnected(true);
+          setWalletAddress(address);
+          setStatus('Conectado');
         }
-        return obj as { connectedStatus: boolean; status: string; address?: any; }
       } catch (error) {
-        return {
-          connectedStatus: false,
-          status: "Erro durante a conexão com a conta",
-        }
+        console.error(error);
+        setStatus('Erro durante a conexão com a conta');
       }
     } else {
-      return {
-        connectedStatus: false,
-        status:
-          "Instale a Metamask no seu browser: https://metamask.io/download.html",
-      }
+      setStatus('Instale o MetaMask no seu navegador');
     }
-  }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', (accounts) => {
+        if (accounts && accounts.length > 0) {
+          const address = accounts[0];
+          setConnected(true);
+          setWalletAddress(address);
+        } else {
+          setConnected(false);
+          setWalletAddress('');
+          setStatus('');
+        }
+      });
+    }
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
-      <h1 className="text-4xl font-bold text-white mb-8">Conectar com o Metamask</h1>
+      <h1 className="text-4xl font-bold text-white mb-8">Conectar com o MetaMask</h1>
       <button
-        onClick={connectWalletPressed}
+        onClick={connectWallet}
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none"
       >
         {isConnected ? 'Conta Conectada' : 'Conectar'}
       </button>
       {isConnected && (
         <p className="text-white mt-4">
-          Conta: {String(walletAddress).substring(0, 5)}...
-          {String(walletAddress).substring(38)}
+          Conta: {walletAddress.substring(0, 5)}...{walletAddress.substring(38)}
         </p>
       )}
       {status && <p className="text-white mt-4">{status}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
