01ecf9e8a7c16e48397518af05f964576355b94a
elisonmartins <martinselison10@gmail.com>
2023-05-23 17:00:23 -0300

    style: atualizando estilo do card do APOD

--- components/Apod/index.tsx
+++ components/Apod/index.tsx
@@ -31,23 +31,25 @@ const Nasa = () => {
   }, []);
 
   return (
-    <div className="mx-10 mt-8 text-center h-90 w-64 bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="mx-10 mt-8 text-center h-90 w-64 bg-white shadow-2xl rounded-lg overflow-hidden">
       {imageData ? (
-        <>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg relative">
 
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <div className='relative'>
             <img className="w-full" src={imageData.url} alt={imageData.title} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Imagem do dia:<br />{imageData.title}</div>
-              <p className="text-gray-700 text-base">
-                {imageData.explanation.length > 200
-                  ? `${imageData.explanation.slice(0, 200)}...`
-                  : imageData.explanation}
-              </p>
-
+            <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent py-2">
+              <h1 className="text-xl text-white text-center font-bold">Imagem do dia</h1>
             </div>
           </div>
-        </>
+          <div className="px-6 py-4">
+            <h1 className="font-bold text-xl mb-2">{imageData.title}</h1>
+            <p className="text-gray-700 text-base">
+              {imageData.explanation.length > 200
+                ? `${imageData.explanation.slice(0, 200)}...`
+                : imageData.explanation}
+            </p>
+          </div>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
